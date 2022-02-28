036b1dd72e9f9bf6629490cb608a1c4d4c7d76bd
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-03-01 01:20:48 +0200

    add: loading progress when routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
+import Router from "next/router";
+import NProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  NProgress.configure({
+    showSpinner: false,
+  });
+
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
   return (
     <>
       <ChakraProvider>
