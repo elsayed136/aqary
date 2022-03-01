b897c719bf64e8c0e942c7479522a045a5fa2648
asema <asema@LAPTOP-B1IQUE15>
2022-03-01 16:57:28 +0200

    Add Properties

---
+++ pages/properties.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import Property from './Property';
+
+const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+    <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p='5'>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+      <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
+      <Button fontSize='xl' >
+        <Link href={linkName}><a>{buttonText}</a></Link>
+      </Button>
+    </Box>
+  </Flex>
+  )
+// const Properties = () => {
+//     return (
+
+    const Properties = () => {
+        return (
+        
+        <Box>
+     <Banner
+      purpose='RENT A HOME'
+      title1='Rental Homes for'
+      title2='Everyone'
+      desc1='Explore from Apartments, builder floors, villas'
+      desc2='and more'
+      buttonText='Explore Renting'
+      linkName='/search?purpose=for-rent'
+      imageUrl='/assets/hero.jpg'
+    />
+        <Flex flexWrap='wrap' justifyContent='center'>
+     <Property/>
+     <Property/>
+     <Property/>
+     <Property/>
+     <Property/>
+     <Property/>
+    </Flex>
+    <Banner
+      purpose='BUY A HOME'
+      title1=' Find, Buy & Own Your'
+      title2='Dream Home'
+      desc1=' Explore from Apartments, land, builder floors,'
+      desc2=' villas and more'
+      buttonText='Explore Buying'
+      linkName='/search?purpose=for-sale'
+      imageUrl='/assets/hero.jpg'
+    />
+     <Flex flexWrap='wrap' justifyContent='center'>
+     <Property/><Property/><Property/><Property/><Property/><Property/>
+    </Flex>
+    </Box>
+    )
+}
+export default Properties;
