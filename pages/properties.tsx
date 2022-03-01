19e2871aed549b33092c31e1eed92d37a3534804
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-03-02 00:04:44 +0200

    fix: type checking

--- pages/properties.tsx
+++ pages/properties.tsx
@@ -1,60 +1,97 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { Flex, Box, Text, Button } from '@chakra-ui/react';
-import Property from './Property';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+import Property from "./Property";
 
-const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
-    <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} alt="banner" />
+interface BannerTypr {
+  purpose: string;
+  title1: string;
+  title2: string;
+  desc1: string;
+  desc2: string;
+  buttonText: string;
+  linkName: string;
+  imageUrl: string;
+}
+
+const Banner: React.FC<BannerTypr> = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => (
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    <Image src={imageUrl} width={500} height={300} alt='banner' />
     <Box p='5'>
-      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
-      <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
-      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
-      <Button fontSize='xl' >
-        <Link href={linkName}><a>{buttonText}</a></Link>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>
+        {purpose}
+      </Text>
+      <Text fontSize='3xl' fontWeight='bold'>
+        {title1}
+        <br />
+        {title2}
+      </Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
+      <Button fontSize='xl'>
+        <Link href={linkName}>
+          <a>{buttonText}</a>
+        </Link>
       </Button>
     </Box>
   </Flex>
-  )
+);
 // const Properties = () => {
 //     return (
 
-    const Properties = () => {
-        return (
-        
-        <Box>
-     <Banner
-      purpose='RENT A HOME'
-      title1='Rental Homes for'
-      title2='Everyone'
-      desc1='Explore from Apartments, builder floors, villas'
-      desc2='and more'
-      buttonText='Explore Renting'
-      linkName='/search?purpose=for-rent'
-      imageUrl='/assets/hero.jpg'
-    />
-        <Flex flexWrap='wrap' justifyContent='center'>
-     <Property/>
-     <Property/>
-     <Property/>
-     <Property/>
-     <Property/>
-     <Property/>
-    </Flex>
-    <Banner
-      purpose='BUY A HOME'
-      title1=' Find, Buy & Own Your'
-      title2='Dream Home'
-      desc1=' Explore from Apartments, land, builder floors,'
-      desc2=' villas and more'
-      buttonText='Explore Buying'
-      linkName='/search?purpose=for-sale'
-      imageUrl='/assets/hero.jpg'
-    />
-     <Flex flexWrap='wrap' justifyContent='center'>
-     <Property/><Property/><Property/><Property/><Property/><Property/>
-    </Flex>
+const Properties = () => {
+  return (
+    <Box>
+      <Banner
+        purpose='RENT A HOME'
+        title1='Rental Homes for'
+        title2='Everyone'
+        desc1='Explore from Apartments, builder floors, villas'
+        desc2='and more'
+        buttonText='Explore Renting'
+        linkName='/search?purpose=for-rent'
+        imageUrl='/assets/hero.jpg'
+      />
+      <Flex flexWrap='wrap' justifyContent='center'>
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+      </Flex>
+      <Banner
+        purpose='BUY A HOME'
+        title1=' Find, Buy & Own Your'
+        title2='Dream Home'
+        desc1=' Explore from Apartments, land, builder floors,'
+        desc2=' villas and more'
+        buttonText='Explore Buying'
+        linkName='/search?purpose=for-sale'
+        imageUrl='/assets/hero.jpg'
+      />
+      <Flex flexWrap='wrap' justifyContent='center'>
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+      </Flex>
     </Box>
-    )
-}
+  );
+};
 export default Properties;
